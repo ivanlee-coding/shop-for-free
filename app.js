11eb20e4319b5699d313ca0059b25f92a18ba1e2
Ivan LEE <ivanlee@example.com>
2022-11-06 14:40:43 +0800

    Enable EJS split big html pages

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const mgo =require('./database/mongoDB');
 const express = require('express');
 const app = express();
 
-console.log(`Program running in: ${process.env.Mode} mode, using port: ${process.env.EXPRESS_SERVER_PORT} ...`);
+app.set('view engine', 'ejs');
 // mgo.connect();
 // mgo.update();
 
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {res.render('index');});
+
 app.listen(process.env.EXPRESS_SERVER_PORT);
 
-app.use(express.static('www'));
+console.log(`Program running in: ${process.env.Mode} mode, using port: ${process.env.EXPRESS_SERVER_PORT} ...`);
+// app.use(express.static('www'));
